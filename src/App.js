deb5184cfc15b3e7b47f2c80e8cfd1f1a355751a
Nicostudent <rojo.nico@hotmail.com>
2022-12-13 13:59:49 -0300

    video responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import { Contador } from "./components/contador";
-import { Titulo } from "./components/titulo.jsx";
-import styles from "../src/styles/app.module.css";
+import React, { useEffect, useState } from "react";
+import './index.css'
+import smoke from './audios/smoke.mp4'
+import mobile from './audios/mobile.mp4'
 
-function App() {
+
+
+
+const Player = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 800);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 800);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
-    <div className={styles.app_container}>
-      <Titulo />
-      <Contador />
+    <div>
+      {isDesktop ? (
+        <video autoPlay loop muted className="bg-vid">
+        <source  src={smoke} type="video/mp4" />
+       </video> ) : (
+         <video autoPlay loop muted className="bg-vid">
+        <source  src={mobile} type="video/mp4" /> 
+       </video>
+          )
+      }
     </div>
   );
 }
-
-export default App;
+export default Player;
